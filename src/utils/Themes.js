fecee5815f37ba37b2c17630a20da218a0e719ac
Nikk-27 <nktllwn@gmail.com>
2024-05-06 01:34:15 -0700

    first commit with current progress of portfolio 2.0

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#090917",
+  bgLight: "#1C1E27",
+  primary: "#854CE6",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#171721",
+  card_light: "#191924",
+  button: "#854CE6",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#fdfdfd",
+  primary: "#007fff",
+  text_primary: "#000000",
+  text_secondary: "#000000",
+  card: "#FFFFFF",
+  button: "#000000",
+};
